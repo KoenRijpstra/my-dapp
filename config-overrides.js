46eb50d4511737ddadf175997656f26b42beeedf
Koen Rijpstra <k.rijpstra@gmail.com>
2023-03-07 16:34:36 +0100

    Enable cjs

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,21 @@
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
-module.exports = function override(config, env) {
+module.exports = function override(config) {
   // do stuff with the webpack config...
-  config.plugins.push(new NodePolyfillPlugin())
+  config.plugins.push(new NodePolyfillPlugin());
+
+  config.module.rules = config.module.rules.map((rule) => {
+    if (rule.oneOf instanceof Array) {
+      rule.oneOf[rule.oneOf.length - 1].exclude = [/\.(js|mjs|jsx|cjs|ts|tsx)$/, /\.html$/, /\.json$/];
+    }
+    return rule;
+  });
+
   return {
     ...config,
     ignoreWarnings: [
       {
-		// Ignore 'failed to parse source map' warnings
+		    // Ignore 'failed to parse source map' warnings
         module: /node_modules\/(moralis|@moralisweb3)/,
       },
     ],
